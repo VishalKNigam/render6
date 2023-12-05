8dc224386a8099d1e49c7d081a7dde1759485c22
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2023-12-05 18:08:06 +0530

    changes

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const { connection } = require("./db");
 const { userRouter } = require("./route/userRoute");
 const cors = require("cors")
 const app = express();
-
+const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors());
 
 app.use("/contacts" , userRouter)
 
-app.listen(8080 , async()=>{
+app.listen(PORT , async()=>{
     try {
         await connection;
         console.log("server is running at port 8080")
